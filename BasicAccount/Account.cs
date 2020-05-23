492aefd682fc7b46b9acc887ca10c5fddaf08ca6
David Almeida <davidalmeidadrive@gmail.com>
2020-05-23 07:27:14 -0300

    Class

---
+++ BasicAccount/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BasicAccount
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; private set; }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return "Account: " + Number + ", Holder: " + Holder + ", Balance : $ " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
